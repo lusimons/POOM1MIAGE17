a23f6ed79a53c3fe1bbc30fa36d0fdaf9471c44a
Alexandre Petit-Pas <petitpasalexandre@gmail.com>
2017-09-12 00:10:41 +0200

    add Translatable interface

---
+++ src/fr/parisnanterre/miage/poa/shapes/Translatable3D.java
@@ -0,0 +1,11 @@
+package fr.parisnanterre.miage.poa.shapes;
+
+
+public interface Translatable3D extends Translatable2D
+{
+    /**
+     * [abstract method getRefPoint]
+     * @return [Point3D object]
+     */
+    public abstract Point3D getRefPoint();
+}
