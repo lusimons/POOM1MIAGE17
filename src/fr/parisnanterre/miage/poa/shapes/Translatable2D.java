cfde7e9943b4e4a18e13d1ac33a711b4e9969078
Alexandre Petit-Pas <petitpasalexandre@gmail.com>
2017-09-11 21:57:20 +0200

    add comments Translatable2D

--- src/fr/parisnanterre/miage/poa/shapes/Translatable2D.java
+++ src/fr/parisnanterre/miage/poa/shapes/Translatable2D.java
@@ -3,5 +3,9 @@ package fr.parisnanterre.miage.poa.shapes;
 
 public interface Translatable2D
 {
+    /**
+     * [abstract method getRefPoint]
+     * @return [Point2D object]
+     */
     public abstract Point2D getRefPoint();
 }
