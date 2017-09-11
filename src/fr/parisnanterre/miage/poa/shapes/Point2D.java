6861ddf96a7d001bc0f0a85652c8f0b43ac3841d
Alexandre Petit-Pas <petitpasalexandre@gmail.com>
2017-09-11 20:38:04 +0200

    add Point2D methods

--- src/fr/parisnanterre/miage/poa/shapes/Point2D.java
+++ src/fr/parisnanterre/miage/poa/shapes/Point2D.java
@@ -1,8 +1,6 @@
 package fr.parisnanterre.miage.poa.shapes;
 
-/**
- * c avec du vert => classe abstraite
- */
+
 public class Point2D extends Parent implements Interface
 {
     private double x;
@@ -16,5 +14,39 @@ public class Point2D extends Parent implements Interface
         this.x = x;
         this.y = y;
     }
-    
+
+    /**
+     * [translate the Point2D with dist_x in abscissa and dist_y in ordinate]
+     * @param double dist_x [move the x point to dist_x in the abscissa]
+     * @param double dist_y [move the y point to dist_x in the ordinate]
+     */
+    public void translate(double dist_x, double dist_y)
+    {
+        this.x += dist_x;
+        this.y += dist_y;
+    }
+
+    /**
+     * [Check if the point is at the origin of the plane]
+     * @return [boolean: true if at the Origin, false otherwise]
+     */
+    public boolean isOrigin()
+    {
+        if (this.x==0 && this.y==0)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * [Calculate the distance between two Point2D]
+     * @param  Point2D point         [Point2D object]
+     * @return         [double]
+     */
+    public double distance(Point2D point)
+    {
+        dist = sqrt( pow(this.x - point.x) + pow(this.y - point.y) );
+        return dist;
+    }
 }
