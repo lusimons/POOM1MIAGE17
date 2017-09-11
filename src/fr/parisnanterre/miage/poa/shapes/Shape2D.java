6b0b68950207e15faf56c86b51ac203082a506b5
Alexandre Petit-Pas <petitpasalexandre@gmail.com>
2017-09-11 22:12:39 +0200

    add Shape2D class, its getter, setter and constructor

---
+++ src/fr/parisnanterre/miage/poa/shapes/Shape2D.java
@@ -0,0 +1,38 @@
+package fr.parisnanterre.miage.poa.shapes;
+
+
+public abstract class Shape2D
+{
+    public Point2D refPoint;
+
+    /**
+	* Default empty Shape2D constructor
+	*/
+	public Shape2D() {
+		this(new Point2D(0.0,0.0));
+	}
+
+	/**
+	* Default Shape2D constructor
+	*/
+	public Shape2D(Point2D refPoint) {
+		super();
+		this.refPoint = refPoint;
+	}
+
+	/**
+	* Returns value of refPoint
+	* @return Point2D
+	*/
+	public Point2D getRefPoint() {
+		return refPoint;
+	}
+
+	/**
+	* Sets new value of refPoint
+	* @param Point2D
+	*/
+	public void setRefPoint(Point2D refPoint) {
+		this.refPoint = refPoint;
+	}
+}
