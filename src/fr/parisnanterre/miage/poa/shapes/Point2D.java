164615f4f657047d4f1a3dcf524adac042a4c4c5
Alexandre Petit-Pas <petitpasalexandre@gmail.com>
2017-09-11 20:12:47 +0200

    change int variables to double

--- src/fr/parisnanterre/miage/poa/shapes/Point2D.java
+++ src/fr/parisnanterre/miage/poa/shapes/Point2D.java
@@ -1,17 +1,20 @@
 package fr.parisnanterre.miage.poa.shapes;
 
+/**
+ * c avec du vert => classe abstraite
+ */
 public class Point2D extends Parent implements Interface
 {
-    private int x;
-    private int y;
+    private double x;
+    private double y;
 
     /**
     * Default Point2D constructor
     */
-    public Point2D(int x, int y) {
-    super();
-    this.x = x;
-    this.y = y;
+    public Point2D(double x, double y) {
+        super();
+        this.x = x;
+        this.y = y;
     }
-
+    
 }
