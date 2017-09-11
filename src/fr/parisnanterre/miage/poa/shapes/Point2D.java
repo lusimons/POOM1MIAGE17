7fa71df0d1ba40ac4e22946173f050b6fed5779a
Alexandre Petit-Pas <petitpasalexandre@gmail.com>
2017-09-11 17:44:10 +0200

    Ajout du constructeur Point2D

---
+++ src/fr/parisnanterre/miage/poa/shapes/Point2D.java
@@ -0,0 +1,17 @@
+package fr.parisnanterre.miage.poa.shapes;
+
+public class Point2D extends Parent implements Interface
+{
+    private int x;
+    private int y;
+
+    /**
+    * Default Point2D constructor
+    */
+    public Point2D(int x, int y) {
+    super();
+    this.x = x;
+    this.y = y;
+    }
+
+}
