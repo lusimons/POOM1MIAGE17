96486ab1b0f85ebf0693ecb76444e38cccb082c3
Alexandre Petit-Pas <petitpasalexandre@gmail.com>
2017-09-12 09:08:23 +0200

    add Square class

---
+++ src/fr/parisnanterre/miage/poa/shapes/Square.java
@@ -0,0 +1,12 @@
+package fr.parisnanterre.miage.poa.shapes;
+
+
+public final class Square extends Rectangle
+{
+    /**
+	* Default empty Square constructor
+	*/
+	public Square() {
+		super();
+	}
+}
